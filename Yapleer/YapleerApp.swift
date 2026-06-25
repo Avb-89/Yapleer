b027354fe6568c9ba8edae34fc86cc623f453bbb
Sitis <darkarhont@icloud.com>
2026-06-25 12:20:07 +0300

    Initial Commit

---
+++ Yapleer/YapleerApp.swift
@@ -0,0 +1,17 @@
+//
+//  YapleerApp.swift
+//  Yapleer
+//
+//  Created by SITIS on 6/25/26.
+//
+
+import SwiftUI
+
+@main
+struct YapleerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
